da91b0a8cbc2e124e30546233cd5dcb708adcbd5
Alisson Zampietro <alisson.zampietro@bluepanda.pt>
2021-07-04 01:52:09 +0100

    feat: add size tracker

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import { Request, Response, Application } from 'express';
 import path from 'path';
-import {createWriteStream} from 'fs';
+import {createWriteStream, stat, createReadStream} from 'fs';
 
 export default function Routes(app:Application) {
     app.get('/health', (req:Request, res:Response) => {
@@ -12,9 +12,22 @@ export default function Routes(app:Application) {
         console.log(filePath)
         const stream = createWriteStream(filePath);
 
+        let uploadedSize = 0;
+        req.on('data', (buffer) => {
+            let length = buffer.length;
+
+            uploadedSize += length;
+
+            const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+            const i = Math.floor(Math.log(uploadedSize) / Math.log(1024));
+            
+            console.log(parseFloat((uploadedSize / Math.pow(1024, i)).toFixed(2)) + ' ' + sizes[i])
+
+        })
+
         stream.on('open', () => req.pipe(stream));
 
-        res.end('deploy done');
+        stream.on('close', () => res.end(JSON.stringify({message: 'file uploaded'})));
     
     })
 }
